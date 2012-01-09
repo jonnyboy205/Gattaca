6e39dff27eff6114b542df754e1c8ee41711f317
Jonathan Bourdett <jab2279@columbia.edu>
2012-01-09 11:57:02 -0500

    read the dna_strand, as well as third line g

--- GattacaTester.py
+++ GattacaTester.py
@@ -9,13 +9,25 @@ max_line_length = float(80.0)
 FILE = open(sys.argv[1] ,"r")
 
 #read in first line
-first_line = float(FILE.readline())
-ceiling = math.ceil(first_line/max_line_length)
+n = int(FILE.readline())
+ceiling = math.ceil(n/max_line_length)
+
+#read in next ceiling lines
+i = int(0)
+dna_strand = ''
+while(i<ceiling):
+	dna_strand += FILE.readline().strip()
+	i+=1
+
+g = int(FILE.readline())
 
 if (DEBUG):
-	print 'DEBUG: %r' % (DEBUG)
-	print 'first_line: %g' % (first_line)
-	print 'max_line_length: %g' % (max_line_length)
-	print 'ceiling: %g' % (ceiling)
+        print 'DEBUG: %r' % (DEBUG)
+        print 'n: %g' % (n)
+        print 'max_line_length: %g' % (max_line_length)
+        print 'ceiling: %g' % (ceiling)
+	print 'dna_strand: %r' % (dna_strand)
+	print 'g: %g' % (g)
 
+j = int(0)
 
